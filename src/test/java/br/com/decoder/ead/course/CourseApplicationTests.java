e87027aea0c178d6d4dd99e3924092f5e534cb69
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2023-05-07 19:31:03 -0300

    Inicio do Projeto Course

---
+++ src/test/java/br/com/decoder/ead/course/CourseApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.decoder.ead.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
