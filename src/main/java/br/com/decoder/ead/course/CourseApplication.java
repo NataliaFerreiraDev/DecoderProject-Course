6bbb1b9a4c06d5b7d5c2ef98b91a858ed2ad6fa1
Natalia Ferreira <nataliaferreira.dev@yahoo.com.br>
2023-07-31 07:59:24 -0300

    Inclusao do service-registry

--- src/main/java/br/com/decoder/ead/course/CourseApplication.java
+++ src/main/java/br/com/decoder/ead/course/CourseApplication.java
@@ -2,8 +2,10 @@ package br.com.decoder.ead.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class CourseApplication {
 
 	public static void main(String[] args) {
